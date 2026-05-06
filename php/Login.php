c87a3b737d88d986efb8399abbdd90cc83624ed4
brix Allen pascua <shy014336@gmail.comshy014336@gmail.comgit config --global user.name>
2026-05-06 15:29:57 +0800

    Changes

---
+++ php/Login.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $password = $_POST['password'];
+
+    if (!empty($email) && !empty($password)) {
+        echo "Login attempt for: ". htmlspecialchars($email);
+    } else {
+        echo "Please fill in all fields"
+    }
+}
+?>
